f7e5bc79de71a959e31f9683bdeff0a4bcd0a3cf
Mauricio <mauricio.o.mesquita@gmail.com>
2013-05-06 22:37:39 -0300

    06_04_2013

--- config/General.php
+++ config/General.php
@@ -11,6 +11,7 @@ $PATHLAYOUT 	 = $ROOTWEB."layout/";
 $PATHMODULOS 	 = $ROOTWEB."layout/modulos/";
 $PATHCSS 	     = "http://www.cupomsocialclube.com.br/desenvolvimento2/layout/css/";
 $PATHDB 	     = $ROOTWEB."lib/db/";
+$PATHLIB         = $ROOTLIB."lib/";
 
 include_once $PATHDB.'Config.php';
 include_once $PATHDB.'Mysql.php';
--- layout/modulos/topo.php
+++ layout/modulos/topo.php
@@ -1,8 +1,15 @@
 <div id="top">
     <div id="search-field">
         <form>
-            <input id="search_form" type="text" value="" id="inputString" onKeyUp="lookup(this.value);" onBlur="fill();" />
+            <input id="search_form" type="text" value="" onKeyUp="lookup(this.value);" onBlur="fill();" />
+            <div id="dropdown">
+
+            </div>
             <button id="button-buscar" type="submit">BUSCAR</button>
         </form>
     </div>
 </div>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="<?=$PATHLIB?>js/primary.js"></script>
